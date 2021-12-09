e074fd9ed5073643a698db1e3ff1f1819d73153c
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2021-12-09 11:53:28 -0800

    connection and routes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose');
+require("./connection/mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 3002;
@@ -10,12 +10,8 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
 
-// Use this to log mongo queries being executed!
-mongoose.set('debug', true);
+
+
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
