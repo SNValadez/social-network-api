f3a203f1241ffc3ce39f65cddb48d46c67e920a5
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2021-12-10 15:42:24 -0800

    create reaction object

--- models/Thought.js
+++ models/Thought.js
@@ -30,6 +30,38 @@ const ThoughtSchema = new Schema(
 
 
 
+  const ReactionSchema = new Schema(
+    {
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal),
+      },
+    },
+    {
+      toJSON: {
+        getters: true,
+      },
+    }
+  );
+
+  thoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+  });
+
 const Thought = model("Thought", ThoughtSchema);
 
 module.exports = Thought;
