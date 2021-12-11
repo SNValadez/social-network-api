0b48dc111881d26756859951d648499615aa0a00
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2021-12-10 17:34:39 -0800

    user add and delete

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,7 +2,7 @@ const {User} = require('../models');
 
 const userController = {
     
-    // Create a new User
+    // Create a new User, brah
     createUser({body}, res) {
         User.create(body)
         .then(dbUserDa => res.json(dbUserDa))
@@ -32,8 +32,50 @@ const userController = {
         })
     },
 
+    getAllUsers(req, res) {
+        User.find({})
+          .populate({
+            path: "thoughts",
+            select: "-__v",
+          })
+          .select("-__v")
+          .sort({ _id: -1 })
+          .then((dbUserDa) => res.json(dbUserDa))
+          .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+          });
+      },
 
-    
-}
+      updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, 
+            { new: true, runValidators: true })
+          .then(dbUserDa => {
+            if (!dbUserDa) {
+              res.status(404).json(
+                  { message: 'Sorry! No user found with this id!' });
+              return;
+            }
+            res.json(dbUserDa);
+          })
+          .catch(err => res.status(400).json(err));
+      },
+      
+          deleteUser({ params }, res) {
+          User.findOneAndDelete({ _id: params.id })
+            .then(dbUserDa => {
+              if (!dbUserDa) {
+                res.status(404).json(
+                    { message: 'No user found with this id! Sorry!' });
+                return;
+              }
+              res.json(dbUserDa);
+            })
+            .catch(err => res.status(400).json(err));
+        }
+      
+
+
+};
 
 module.exports = userController; 
