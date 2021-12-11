2d377f9157d2d51237b3696d9bef05a423460d9e
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2021-12-10 16:17:30 -0800

    getThoughtById update

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -21,6 +21,26 @@ const thoughtController = {
     .catch(err => res.json(err)); 
     },
 
+    getThoughtById({params}, res) {
+        Thought.findOne(
+            { _id: params.id })
+        .populate({path: 'reactions',
+        select: '-__v'})
+        .select('-__v')
+        .then(dbThoughtDa => {
+            if(!dbThoughtDa) {
+            res.status(404).json(
+                {message: 'No thoughts with this ID!'});
+            return;
+        }
+        res.json(dbThoughtDa)
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+
 
 
 
