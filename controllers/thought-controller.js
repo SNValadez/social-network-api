48f3a7205ae1fff8073fcc3d2eddd810309231b4
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2021-12-10 16:44:42 -0800

    add reactions

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -84,6 +84,45 @@ const thoughtController = {
             })
             .catch(err => res.status(400).json(err));
     },
+
+
+    addReaction({params, body}, res) {
+        Thought.findOneAndUpdate(
+            {_id: params.thoughtId}, 
+            {$push: {reactions: body}}, 
+            {new: true, runValidators: true})
+        .populate(
+            {path: 'reactions', select: '-__v'})
+        .select('-__v')
+        .then(dbThoughtDa => {
+        if (!dbThoughtDa) {
+            res.status(404).json(
+                {message: 'No thoughts with this ID! Sorry!'});
+            return;
+        }
+        res.json(dbThoughtDa);
+        })
+        .catch(err => res.status(400).json(err))
+
+    },
+
+    // Delete a reaction by ID
+    deleteReaction({params}, res) {
+        Thought.findOneAndUpdate(
+            {_id: params.thoughtId}, 
+            {$pull: {reactions: {reactionId: params.reactionId}}}, 
+            {new : true})
+        .then(dbThoughtDa => {
+            if (!dbThoughtDa) {
+                res.status(404).json(
+                    {message: 'Nope! No thoughts with this ID! Try again!'});
+                return;
+            }
+            res.json(dbThoughtDa);
+        })
+        .catch(err => res.status(400).json(err));
+    }
+
 };
 
 
